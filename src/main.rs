f6f8970b613374c8ceee8587810fb40afdd2f6c4
courage <asphyxia123@protonmail.com>
2024-04-19 11:18:27 +0200

    todo: transfer vec into tree

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 
 pub mod process;
 
+use std::collections::HashMap;
+
 use process::{TreeNode,ZProcess};
 
 use sysinfo::{
@@ -9,11 +11,22 @@ use sysinfo::{
 };
 
 fn main() {
-    let mut sys_handle = System::new();
+    let mut sys_handle = System::new_all();
 
-    let zprocesses = sys_handle.processes().into_iter().map(|(_,sys_proc)|{
+    let mut zprocesses = sys_handle.processes().into_iter().map(|(_,sys_proc)|{
         ZProcess::new(sys_proc)
     }).collect::<Vec<ZProcess>>();
 
-    dbg!(zprocesses.len());
+    let mut root_proc_idx = 0;
+    for (idx, proc) in zprocesses.iter().enumerate() {
+        if proc.pid == 1 {
+            root_proc_idx = idx;
+        }
+    }
+    if root_proc_idx == 0 {panic!()}// TODO: remove later
+
+    let mut root = TreeNode::new_with(
+        zprocesses.swap_remove(root_proc_idx), 
+        HashMap::new()
+    );
 }
