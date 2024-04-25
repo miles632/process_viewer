fd8509b057374c71bdcd2530ccb6a044124d576f
miles632 <asphyxia123@protonmail.com>
2024-04-25 23:17:42 +0200

    extra gui stuff

--- src/gui.rs
+++ src/gui.rs
@@ -14,6 +14,44 @@ impl EguiApp {
             ptree: proc_vec,
         }
     }
+
+    fn output_procinfo(&mut self, ui: &mut egui::Ui, num_processes: &usize) {
+        ui.add_space(4.0);
+
+        let font_id = egui::TextStyle::Body.resolve(ui.style());
+        let row_height = ui.fonts(|f| f.row_height(&font_id) + ui.spacing().item_spacing.y);
+
+        ScrollArea::vertical()
+            .auto_shrink(false)
+            .show_viewport(ui, |ui,viewport| {
+                ui.set_height(row_height * *num_processes as f32);
+
+                let first_item = (viewport.min.y / row_height).ceil() as usize;
+                let last_item = (viewport.max.y / row_height).ceil() as usize + 1;
+                let last_item = last_item.at_most(*num_processes);
+
+                let mut used_rect = egui::Rect::NOTHING; 
+
+                for i in first_item..last_item {
+                    let identation = (i % 100) as f32;
+                    let x = ui.min_rect().left() + identation;
+                    let y = ui.min_rect().top() + i as f32 * row_height;
+
+                    let text = format!("piss");
+
+                    let text_rect = ui.painter().text(
+                        pos2(x, y), 
+                        egui::Align2::LEFT_TOP, 
+                        text, 
+                        font_id.clone(), 
+                        ui.visuals().text_color(),
+                    );
+                    used_rect = used_rect.union(text_rect);
+                }
+
+                ui.allocate_rect(used_rect, egui::Sense::hover());
+            });
+    }
 }
 
 impl Default for EguiApp {
@@ -27,15 +65,7 @@ impl Default for EguiApp {
 impl eframe::App for EguiApp{
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         // egui::TopBottomPanel::default_height(self, )
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ScrollArea::vertical().auto_shrink([false;2]).show(ui, |ui|{
-                for proc in &mut self.ptree {
-                    ui.horizontal(|ui|{
-                        let label = format!("{:?}", proc.pid);
-                        ui.label(label);
-                    });
-                }
-            });
-        }); 
+        todo!()
     }
+
 }
